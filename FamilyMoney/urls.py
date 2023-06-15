0e1b3c80ae87757441fda7e9af80b86a5691986b
Pushkar Shetye <pushkarshetye803@gmail.com>
2023-06-16 00:44:50 +0530

    first commit

---
+++ FamilyMoney/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.home, name='home'),
+    path('base/', views.base, name='base'),
+    path('add_family_member/<int:user_id>/',
+         views.add_family_member, name='add_family_member'),
+    path('remove_family_member/<int:family_member_id>/',
+         views.remove_family_member, name='remove_family_member'),
+    path('list/', views.list_users, name='list'),
+
+
+]
---
+++ FamilyMoney/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import FamilyMember
+from django.contrib import messages
+from django.http import JsonResponse
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+@login_required
+def home(request):
+    users = User.objects.exclude(id=request.user.id)
+    family_members = FamilyMember.objects.filter(user=request.user)
+    family_member_ids = family_members.values_list('member_id', flat=True)
+    return render(request, 'home.html', {'users': users, 'family_members': family_members, 'family_member_ids': family_member_ids})
+
+
+@login_required
+def add_family_member(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    is_family_member = FamilyMember.objects.filter(
+        user=request.user, member=user).exists()
+    if not is_family_member:
+        family_member = FamilyMember.objects.create(
+            user=request.user, member=user)
+        added_username = family_member.member.username
+        return redirect('home')
+    else:
+        return redirect('home')  # No need for an error message in this case
+
+
+@login_required
+def remove_family_member(request, family_member_id):
+    family_member = get_object_or_404(FamilyMember, id=family_member_id)
+    family_member.delete()
+    return redirect('home')
+
+
+def list_users(request):
+    items = User.objects.all()
+    data = {'items': list(items.values())}
+    return JsonResponse(data)
+
+
+# return logged in user data                                Reading data from db
+# return my family data                                     Reading data from db
+# create a url/api/endpoint/route to add_family_member      Write data from db
+# create a url/api/endpoint/route to remove_family_member   Write data from db
+
+"""
+C Create POST
+R Read   GET
+U Update  PUT
+D Delete DELETE
+"""
